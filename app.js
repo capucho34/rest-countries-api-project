b42ef97584aa06a4a19411b6e3d496dc1a7bbb20
capucho34 <denyairka123@gmail.com>
2026-03-05 22:37:24 +0100

    Added search country (beta)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { renderCountriesList } from "./dom-utils.js";
 console.log("test");
 const API_URL_ALL = "https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,population,region";
 let countries;
-
+let query = "";
 fetch(API_URL_ALL)
     .then((res) => res.json())
     .then((countriesRaw) => {
@@ -17,4 +17,13 @@ fetch(API_URL_ALL)
     });
         console.log(countries);
         renderCountriesList(countries);
+});
+
+document.querySelector('#query').addEventListener("input", (e) => {
+    const query = e.target.value;
+    countries = countries.filter((country) =>
+        country.name.toLowerCase().includes(query.toLowerCase())
+);
+    console.log("change", e.target.value);
+    renderCountriesList(countries);
 });
